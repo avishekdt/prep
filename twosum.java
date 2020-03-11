91dbd2cf9ac175539a7e9d23af3b4bcd4b67977d
avdatta <adatta.mid@gmail.com>
2020-03-11 17:00:07 +0530

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -1,9 +1,27 @@
 package test;
 
+import java.util.HashMap;
+
 public class twosum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int [] arr =  {1,3,4,5,8,9};
+		int target = 10;
+		
+		test(arr, target);
+
+	}
+
+	private static void test(int[] arr, int target) {
+		HashMap<Integer, Integer> hm =  new HashMap<Integer, Integer>();	
+		for (int i =0; i < arr.length; i ++) {
+			if(hm.containsKey(arr[i]))
+			{
+				System.out.println(arr[i]+ " and " + hm.get(arr[i]));
+			}
+			else 
+				hm.put(target - arr[i], arr[i]);
+		}
 
 	}
 
